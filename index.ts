1515a3e25750d61eea948c6a484bb361d085755c
zeeshanshabbeer <z6680362@gmail.com>
2022-09-15 17:37:08 +0500

    lambda

---
+++ index.ts
@@ -0,0 +1,18 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResultV2,
+  Handler,
+} from "aws-lambda";
+import * as _ from "lodash";
+
+export const handler: Handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResultV2> => {
+  const max = 10;
+  const val = _.random(max);
+  const res = {
+    statusCode: 200,
+    body: `random value max ${max}: value ${val}`,
+  };
+  return res;
+};
